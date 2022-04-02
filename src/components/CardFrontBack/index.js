7331bccc4e6025e7f5928412b58bfa3aab9bc47c
immaioli <irneu_marcelo@hotmail.com>
2022-04-01 23:51:18 -0300

    :sparkles: Adiciona o evento de click no componente CardFronBack

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,8 +2,17 @@ import CardGame from '../CardGame';
 import './style.css';
 
 const CardFrontBack = () => {
+  window.cardFrontBack = {};
+  window.cardFrontBack.handleClick = (event) => {
+    event.preventDefault();
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest('.card-front-back');
+
+    $cardFrontBack.classList.toggle('-active');
+  }
+
   return /*html*/`
-    <article class="card-front-back">
+    <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
       <div class="card -front">
         ${CardGame()}
       </div>
