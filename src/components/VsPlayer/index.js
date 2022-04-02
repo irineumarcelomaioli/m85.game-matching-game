17a9cc533fcb4f136957fceb7100147d154670d5
immaioli <irneu_marcelo@hotmail.com>
2022-04-02 06:35:37 -0300

    :construction: Criado o componente VsPlayer sem o css

---
+++ src/components/VsPlayer/index.js
@@ -0,0 +1,7 @@
+const VsPlayer = (content = "vs") => {
+  return /*html*/`
+    <span class="vs-player">${content}</span>
+  `
+}
+
+export default VsPlayer;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import PlayerName from '../../components/PlayerName';
+import VsPlayer from '../../components/VsPlayer';
 import './style.css';
 
 const ScoreBoard = () => {
   return /*html*/`
     <header class="score-board">
       ${PlayerName("Player 01")}
+      ${VsPlayer()}
       ${PlayerName("Player 02")}
     </header>
   `;
