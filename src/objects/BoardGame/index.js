51336a9efe8d5cc8ced79d0d0b5b3f473eb6be5a
immaioli <irneu_marcelo@hotmail.com>
2022-04-05 23:59:54 -0300

    :bug: Troca a vez de jogar do jogandor após virar duas cartas.

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,14 +3,30 @@ import CardFrontBack from '../../components/CardFrontBack';
 import cards from './data';
 
 const BoardGame = (amountCards) => {
-  // const $htmlContent = $htmlCardFrontBack.repeat(amountCards);
-  
-  
+  const flipAndHideCards = ( $cardsActive) => {
+    $cardsActive.forEach((card) => card.classList.remove('-active'));
+  }
+  const swapPlayer = ($arrowDown) => {
+    const currentPlayer = $arrowDown.getAttribute('data-currentPlayer');
+    $arrowDown.setAttribute('data-currentPlayer', currentPlayer == 1 ? 2 :1 );
+  }
+  window.boardGame = {};
+  window.boardGame.handleClick = () => { 
+    const $boardGame = document.querySelector('.board-game');
+    const $arrowDown = document.querySelector('.arrow-down');
+    const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active');
+    if ($cardsActive.length === 2) {
+      setTimeout(() => {
+        flipAndHideCards($cardsActive);
+        swapPlayer($arrowDown);
+      }, 1000);
+    }
+  }
   const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, cards.altIcon));
   const $htmlCards = htmlCardsList.join('');
 
   return /*html*/`
-    <section class="board-game">
+    <section class="board-game" onClick="boardGame.handleClick()">
       ${$htmlCards}
     </section>    
   `;
